6a5a1ec4f319b480df3dc613a2dc48941ad1d25b
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-08-15 12:42:47 +1000

    continuing work on tabs system

--- code/components/tabs-screen/tabs-form/tabsForm.tsx
+++ code/components/tabs-screen/tabs-form/tabsForm.tsx
@@ -3,19 +3,31 @@ import React, { useContext, useState } from "react"
 
 export const TabsForm: React.FC = () => {
   const tabContext = useContext(TabsContext)
-  const [tabName, setTabName] = useState("new Tab")
+  const [tabName, setTabName] = useState("dbTabName")
+  const [tabData, setTabData] = useState<string>("dbTabData");
   const handleTabName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTabName(event.target.value);
 
   }
-  return (
-    <div className="min-w-1/2 h-full">
-      <form>
+  const handleTabData = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTabData(event.target.value)
+  }
+  const preventEnter = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+  return tabContext?.tabs.length == 0 ? (
+    <>
+      {/* no tabs create new tab component */}
+      {/* */}
+    </>
+  ) :
+    (<div className="min-w-1/2 h-full">
+      <form onSubmit={preventEnter}>
         <label htmlFor="tab-input">Tab Name : </label>
         <input type="text" id="tab-input" value={tabName} onChange={handleTabName} />
         <hr />
-        <textarea name="" id=""></textarea>
+        <textarea name="" className="min-w-1/2 h-full" value={tabData} onChange={handleTabData}></textarea>
       </form>
-    </div>
-  )
+    </div>)
+
 }
